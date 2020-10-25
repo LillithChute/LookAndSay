95842e487ea3aabff1f5465868f1206dcf794e78
Chute, Lillith <lillithchute@maine.rr.com>
2020-10-25 16:13:11 -0400

    Initial commit of project files.  Also framed out the RIterator interface.

---
+++ src/lookandsay/RIterator.java
@@ -0,0 +1,41 @@
+package lookandsay;
+
+import java.util.Iterator;
+
+/**
+ * This interface represents a generic list.  This interface is also iterable.
+ *
+ * @param <T> The type of data this will work with, represented by T.
+ */
+public interface RIterator<T> extends Iterator<T> {
+
+  /**
+   * This will advance and return the next element.
+   *
+   * @return The next element.
+   */
+  T next();
+
+  /**
+   * This method returns the current number in the sequence and reverts
+   * to the previous number in the sequence.
+   *
+   * @return The current number in the sequence.
+   */
+  T prev();
+
+  /**
+   * should return true if the next number to be returned is still lesser than end
+   * (specified in the constructors), false otherwise.
+   *
+   * @return Either true or false.
+   */
+  boolean hasNext();
+
+  /**
+   * should return true if it is possible to go back one step, false otherwise.
+   *
+   * @return True or false.
+   */
+  boolean hasPrevious();
+}
